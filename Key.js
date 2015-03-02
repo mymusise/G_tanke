04f97e8ad2211d8043f305117b0323eab92418d5
Chengxi Guo <mymusise1@gmail.com>
2015-03-02 23:56:54 +0800

    test

---
+++ Key.js
@@ -0,0 +1,29 @@
+    function KeyDown(e){
+        console.log("key press!");
+        var currKey=0,e=e||event;
+        currKey=e.keyCode||e.which||e.charCode;
+        var keyName = String.fromCharCode(currKey);
+        console.log("currKey:"+currKey+" keyname:"+keyName)
+        if (currKey==37)
+        {
+            tanke1.position.x-=2
+        }
+        if (currKey==39)
+        {
+            tanke1.position.x+=2
+        }
+        if (currKey==38)
+        {
+            tanke1.position.y-=2
+        }
+        if (currKey==40)
+        {
+            tanke1.position.y+=2
+        }
+        if (currKey==83)
+        {
+            fire1=1;
+            init_Fire();
+            
+        }
+    }
---
+++ World.js
@@ -0,0 +1,44 @@
+var doSleep = false;// body can sleep
+var gravity = new b2Vec2(0, 000);// define gravity
+var worldAABB=new b2AABB();
+var timeStep = 1 / 60;
+var iteration = 1;
+function createWorld (argument) {
+	var worldAABB = new b2AABB();
+    worldAABB.minVertex.Set(-400, -400);
+    worldAABB.maxVertex.Set(400, 400);
+    // create world
+    var world = new b2World(worldAABB, gravity, doSleep);
+    return world;
+    // how many iteration for collisions calculations
+}
+function initRender (argument) {
+
+}
+function addBody(sprite, x, y, width, height, density) {
+        var shapeDef = new b2BoxDef();
+        shapeDef.extents.Set(width * 0.5, height * 0.5);
+        var bodyDef = new b2BodyDef();
+        bodyDef.AddShape(shapeDef);
+        bodyDef.position.Set(x, y);
+        if (density) {
+            shapeDef.density = density;
+            shapeDef.friction = 1.4;
+            shapeDef.restitution = 1.2;
+            bodyDef.rotation = 0.1;
+        }
+        body = tankeworld.CreateBody(bodyDef);
+        body.m_userData = sprite;
+    }
+
+function draw() {
+	var body, sprite;
+	for (body = tankeworld.m_bodyList; body; body = body.m_next) {
+	    // console.log(body);
+	    sprite = body.GetUserData();
+	    if (sprite) {
+	        sprite.position = body.GetCenterPosition();
+	        sprite.rotation = body.GetRotation();
+	    }
+}
+}
