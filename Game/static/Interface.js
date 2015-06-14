cea8348cdac5961b7522d9b3f6b1656c27219540
Chengxi Guo <mymusise1@gmail.com>
2015-06-14 21:30:11 +0800

    V0.1
    remove server ,fix bug with the map_class and the style of
    codemirror,adding some interface.

--- Game/static/Interface.js
+++ Game/static/Interface.js
@@ -1,5 +1,3 @@
-
-
 function getData (user) {
 	// body...
 	// console.log(tanke1.GetUserData,tanke2.GetUserData)
@@ -16,4 +14,64 @@ function getData (user) {
 		Enmey_position:tankes[0].position,
 		Enmey_firing:tankes[0].fire.visible
 	}
+}
+
+function go_left () {
+	// body...
+	tanke1.GetUserData().position.x-=2;
+	if(tanke1.m_rotation!=3.1415){
+		var up_texture = PIXI.Texture.fromImage("sources/tanke_left.jpg");
+		// tanke1.GetUserData().setTexture(up_texture);
+        tanke1.m_rotation=3.1415
+		tanke1.GetUserData().direction=3;
+	}
+}
+function go_rigth() {
+	// body...
+	tanke1.GetUserData().position.x+=2;
+	if(tanke1.m_rotation!=0){
+				var up_texture = PIXI.Texture.fromImage("sources/tanke_right.jpg");
+				// tanke1.GetUserData().setTexture(up_texture);
+				tanke1.m_rotation=0;
+                tanke1.GetUserData().direction=1;
+			}
+}
+function go_up() {
+	// body...
+	tanke1.GetUserData().position.y-=2;
+	if(tanke1.m_rotation!=3.1415+3.1415/2){
+				var up_texture = PIXI.Texture.fromImage("sources/tanke_up.jpg");
+				// tanke1.GetUserData().setTexture(up_texture);
+                tanke1.m_rotation=3.1415+3.1415/2;
+				tanke1.GetUserData().direction=0;
+			}
+}
+function go_down () {
+	// body...
+	tanke1.GetUserData().position.y+=2
+	if(tanke1.m_rotation!=3.1415/2){
+		var up_texture = PIXI.Texture.fromImage("sources/tanke_down.jpg");
+		// tanke1.GetUserData().setTexture(up_texture);
+        tanke1.m_rotation=3.1415/2;
+		tanke1.GetUserData().direction=2;
+	}
+}
+function fire (argument) {
+	// body...
+	tanke1.GetUserData().goFire();
+}
+
+function runtanke_test1(){
+var timer = null;
+clearTimeout(timer);
+var j=0;
+for (var i = 0; i < 10; i++,j++) {
+	timer = setTimeout(go_rigth, j*60);
+};
+for (var i = 0; i < 120; i++,j++) {
+	timer = setTimeout(go_up, j*60);
+};
+timer = setTimeout(go_rigth, j*60);
+j++;
+timer = setTimeout(fire, j*60);
 }
